8ce76ff8068ca968691bde7014c813825455ee75
申昊晨 <shenhaochen@mia.com>
2020-04-01 09:39:03 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::get('/getLeaderboard', '\App\Http\Controllers\Api\LeaderboardController@getLeaderboard');    //积分榜
 Route::get('/randomTodayDuty', '\App\Http\Controllers\Api\DutyController@randomDuty');              //随机生成今日值日生
