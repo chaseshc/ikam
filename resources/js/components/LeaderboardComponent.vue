db1e1f9aa096326689d9d892fc032b935c4e7a4a
chase891112 <chase891112@live.cn>
2020-04-10 11:39:57 +0800

    update

--- resources/js/components/LeaderboardComponent.vue
+++ resources/js/components/LeaderboardComponent.vue
@@ -4,41 +4,45 @@
             <img src="/images/leaderboard/logo1.png" alt="">
         </div>
         <div class="list">
-            <div class="item" v-for="(pos, index) in leaderboard_data">
-                <div class="pos" v-if="index === 0">
-                    <img src="/images/leaderboard/first.png" alt="">
-                </div>
-                <div class="pos" v-else-if="index === 1">
-                    <img src="/images/leaderboard/second.png" alt="">
-                </div>
-                <div class="pos" v-else-if="index === 2">
-                    <img src="/images/leaderboard/third.png" alt="">
-                </div>
-                <div class="pos" v-else>
-                    {{index+1}}
-                </div>
+            <div class="item-list">
+                <div class="item" v-for="(pos, index) in leaderboard_data">
+                    <div class="pos" v-if="index === 0">
+                        <img src="/images/leaderboard/first.png" alt="">
+                    </div>
+                    <div class="pos" v-else-if="index === 1">
+                        <img src="/images/leaderboard/second.png" alt="">
+                    </div>
+                    <div class="pos" v-else-if="index === 2">
+                        <img src="/images/leaderboard/third.png" alt="">
+                    </div>
+                    <div class="pos" v-else>
+                        {{index+1}}
+                    </div>
 
-                <div class="pic" :style="pos['head_img']"></div>
-                <div class="name">
-                    {{pos["true_name"]}}
+                    <div class="one-item">
+                        <div class="pic" :style="pos['head_img']"></div>
+                        <div class="name">
+                            {{pos["true_name"]}}
+                        </div>
+                        <div class="today_score">
+                            {{pos["today_score"]}}
+                        </div>
+                        <div class="score">
+                            {{pos["total_score"]}}
+                        </div>
+                        <div class="badge" v-if="pos['reward_icon'] != ''">
+                            <img v-bind:src="pos['reward_icon']" alt="">
+                        </div>
+                    </div>
                 </div>
-                <div class="today_score">
-                    {{pos["today_score"]}}
-                </div>
-                <div class="score">
-                    {{pos["total_score"]}}
-                </div>
-                <div class="badge" v-if="pos['reward_icon'] != ''">
-                    <img v-bind:src="pos['reward_icon']" alt="">
-                </div>
+            </div>
+
+            <div id="leaderboard_update_time-div" v-show="is_show">
+                <span>积分榜更新时间：{{leaderboard_update_time}}</span>
             </div>
         </div>
 
-        <div id="leaderboard_update_time-div">
-            <span>积分榜更新时间：{{leaderboard_update_time}}</span>
-        </div>
-
-        <div class="duty">
+        <div class="duty" v-show="is_show">
             <div class="item">
                 <div class="title">
                     今日值日
@@ -61,7 +65,8 @@
                 leaderboard_data: [],
                 leaderboard_update_time: '',
                 dutyStaffData: [],
-                today_duty_update_time: ''
+                today_duty_update_time: '',
+                is_show:false
             }
         },
         methods: {
@@ -71,7 +76,7 @@
                     if (response.status == 200) {
                         this.leaderboard_data = response.data.leaderboard_data;
                         this.leaderboard_update_time = response.data.leaderboard_update_time;
-
+                        this.is_show = true;
                     } else {
                         console.log('获取积分榜数据失败')
                     }
@@ -84,6 +89,7 @@
                     if (response.status == 200) {
                         this.dutyStaffData = response.data.today_duty_staff;
                         this.today_duty_update_time = response.data.duty_created_at;
+                        this.is_show = true;
                     } else {
                         console.log('获取值日数据失败')
                     }
