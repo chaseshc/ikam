6841c4bb9a7da13251b46e20a2478d415435b407
chase891112 <chase891112@live.cn>
2020-04-10 17:57:42 +0800

    update

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <title>岩锦</title>
     <link rel="stylesheet" href="{{URL::asset('css/leaderboard/normalize.min.css')}}">
-    <link rel="stylesheet" href="{{URL::asset('css/leaderboard/leaderboard.css?v=1.3')}}">
+    <link rel="stylesheet" href="{{URL::asset('css/leaderboard/leaderboard.css?v=1.4')}}">
     <script>
         window.console = window.console || function(t) {};
     </script>
